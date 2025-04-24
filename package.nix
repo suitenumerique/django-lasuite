67616ab1e66502456b1faa290ec91cda9b57c901
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2025-04-24 16:18:53 +0200

    📦(nix) add package

---
+++ package.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  django,
+  djangorestframework,
+  joserfc,
+  mozilla-django-oidc,
+  pyjwt,
+  requests,
+  setuptools,
+  wheel,
+  factory-boy,
+  pytest,
+  pytest-django,
+  responses,
+  ruff,
+}:
+
+buildPythonPackage rec {
+  pname = "django-lasuite";
+  version = "0.0.7";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "suitenumerique";
+    repo = "django-lasuite";
+    tag = "v${version}";
+    hash = "sha256-0UeXMqpk7DPWAGZto7nHhvFK6YsNqywrTFQ/q+7IHDY=";
+  };
+
+  pythonRelaxDeps = true;
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    django
+    djangorestframework
+    joserfc
+    mozilla-django-oidc
+    pyjwt
+    requests
+  ];
+
+  optional-dependencies = {
+    build = [
+      setuptools
+      wheel
+    ];
+    dev = [
+      factory-boy
+      pytest
+      pytest-django
+      responses
+      ruff
+    ];
+  };
+
+  pythonImportsCheck = [
+    "lasuite"
+  ];
+
+  meta = {
+    description = "The common library for La Suite Django projects and Proconnected Django projects";
+    homepage = "https://github.com/suitenumerique/django-lasuite";
+    changelog = "https://github.com/suitenumerique/django-lasuite/blob/${src.tag}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ soyouzpanda ];
+  };
+}
