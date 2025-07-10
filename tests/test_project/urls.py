5ba2dea9c743982efccf21c1a6c27a7997aa4ebd
Manuel Raynaud <manu@raynaud.io>
2025-07-10 15:48:51 +0200

    🐛(tests) fix test_project app to be usable with management command
    The test_project app had missing configuration blocking usage of the
    django admin management commands. We need to use them for generating
    migrations.

--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -2,10 +2,10 @@
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from test_project.oidc_resource_server.views import UserViewSet
 
 from lasuite.oidc_login.urls import urlpatterns as oidc_urls
 from lasuite.oidc_resource_server.urls import urlpatterns as resource_server_urls
-from tests.test_project.oidc_resource_server.views import UserViewSet
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="users")
